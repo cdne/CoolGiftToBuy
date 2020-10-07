1935165e328d515473dfd0e63e9ec7c4588f3e79
Claudiu <cduneata@gmail.com>
2020-10-08 00:10:22 +0300

    Add .gitignore

--- API/Models/TagDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API.Models
-{
-    public class TagDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int ProductId { get; set; }
-    }
-}

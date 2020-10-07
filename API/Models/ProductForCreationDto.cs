8cdb57dee967b48c4b3ab53c376c2ea109fa6ccd
Claudiu <cduneata@gmail.com>
2020-10-08 00:07:21 +0300

    Add dto for creating products on POST

---
+++ API/Models/ProductForCreationDto.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Models
+{
+    public class ProductForCreationDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageSource { get; set; }
+        public string AffiliateLink { get; set; }
+        public string ProductUrl { get; set; }
+        public int CategoryId { get; set; }
+    }
+}

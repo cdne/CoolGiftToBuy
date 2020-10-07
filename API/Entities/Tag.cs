ced0908267202cc8552e3eb8f9a6d94481e4503f
Claudiu <cduneata@gmail.com>
2020-10-08 00:00:29 +0300

    Add tag entity

---
+++ API/Entities/Tag.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities
+{
+    public class Tag
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+
+        public int ProductId { get; set; }
+    }
+}

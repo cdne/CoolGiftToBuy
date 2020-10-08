e072da314b002e35c8bf5300f9f9b23fb8b10193
Claudiu <cduneata@gmail.com>
2020-10-08 11:53:36 +0300

    Add mapper for product DTO

---
+++ API/Profile/ProductProfile.cs
@@ -0,0 +1,38 @@
+﻿using API.Entities;
+using API.Models;
+
+namespace API.Profile
+{
+    public class ProductProfile : AutoMapper.Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDto>()
+                .ForMember(
+                    dest => dest.Name,
+                    opt => opt.MapFrom(src => src.Name)
+                ).ForMember(
+                    dest => dest.Description,
+                    opt => opt.MapFrom(src => src.Description)
+                ).ForMember(
+                    dest => dest.AffiliateLink,
+                    opt => opt.MapFrom(src => src.AffiliateLink)
+                ).ForMember(
+                    dest => dest.ImageSource,
+                    opt => opt.MapFrom(src => src.ImageSource)
+                ).ForMember(
+                    dest => dest.ProductUrl,
+                    opt => opt.MapFrom(src => src.ProductUrl)
+                )
+                .ForMember(
+                    dest => dest.CategoryId,
+                    opt => opt.MapFrom(src => src.CategoryId)
+                ).ForMember(
+                    dest => dest.CategoryName,
+                    opt => opt.MapFrom(src => src.CategoryName)
+                );
+
+            CreateMap<ProductForCreationDto, Product>();
+        }
+    }
+}

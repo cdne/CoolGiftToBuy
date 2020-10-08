807be0fdfac3ba5415cc90631f718d352b109cfe
Claudiu <cduneata@gmail.com>
2020-10-08 11:53:55 +0300

    Add mapp for tag dto

---
+++ API/Profile/TagProfile.cs
@@ -0,0 +1,19 @@
+﻿using API.Entities;
+using API.Models;
+
+namespace API.Profile
+{
+    public class TagProfile :AutoMapper.Profile
+    {
+        public TagProfile()
+        {
+            CreateMap<Tag, TagDto>().ForMember(
+                dest => dest.Name,
+                opt => opt.MapFrom(src => src.Name)
+            ).ForMember(
+                dest => dest.ProductId,
+                opt => opt.MapFrom(src => src.ProductId)
+            );
+        }
+    }
+}

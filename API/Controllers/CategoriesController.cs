3eb038df39b4da082482dba1a0b9355ed225a95a
Claudiu <cduneata@gmail.com>
2020-10-08 11:50:58 +0300

    Add categories controller

---
+++ API/Controllers/CategoriesController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using API.Models;
+using API.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CategoriesController(ICategoryRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult GetAllCategories()
+        {
+            var categories = _repository.GetCategories();
+            return Ok(_mapper.Map<ICollection<CategoryDto>>(categories));
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetCategoryById(int id)
+        {
+            var category = _repository.GetCategoryById(id);
+            return Ok(_mapper.Map<CategoryDto>(category));
+        }
+
+        [HttpGet("{id}/products")]
+        public IActionResult GetAllProductFromCategory(int id)
+        {
+            var productsByCategory = _repository.GetProductsByCategoryId(id);
+            return Ok(_mapper.Map<ICollection<ProductDto>>(productsByCategory));
+        }
+    }
+}

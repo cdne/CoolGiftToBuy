4a880cbb4fd2f69c9ade314a1bb015f35516b578
Claudiu <cduneata@gmail.com>
2020-10-08 11:53:29 +0300

    Add mapper for category DTO

---
+++ API/Profile/CategoryProfile.cs
@@ -0,0 +1,20 @@
+﻿using API.Entities;
+using API.Models;
+
+namespace API.Profile
+{
+    public class CategoryProfile : AutoMapper.Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryDto>()
+                .ForMember(
+                    dest => dest.Name,
+                    opt => opt.MapFrom(src => src.Name)
+                ).ForMember(
+                    dest => dest.Description,
+                    opt => opt.MapFrom(src => src.Description)
+                );
+        }
+    }
+}

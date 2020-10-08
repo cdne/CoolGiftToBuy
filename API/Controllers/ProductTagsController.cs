4db619677c5ac51e2e6d8ea5a99c9875de5fe1e3
Claudiu <cduneata@gmail.com>
2020-10-08 11:51:12 +0300

    Add product tags controller

---
+++ API/Controllers/ProductTagsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using API.Models;
+using API.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/products/{productId}/tags")]
+    public class ProductTagsController : Controller
+    {
+        private readonly IProductTagRepository _productTagRepository;
+        private readonly IMapper _mapper;
+
+        public ProductTagsController(IProductTagRepository productTagRepository, IMapper mapper)
+        {
+            _productTagRepository = productTagRepository;
+            _mapper = mapper;
+        }
+        
+        [HttpGet]
+        public IActionResult GetProductTagsByProductId(int productId)
+        {
+            var allTagsForProduct = _productTagRepository.GetTagsByProductId(productId);
+            
+            return Ok(_mapper.Map<ICollection<TagDto>>(allTagsForProduct));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProductTagByTagId(int id)
+        {
+            var productTagByTagId = _productTagRepository.GetTagById(id);
+            return Ok(_mapper.Map<TagDto>(productTagByTagId));
+        }
+        
+    }
+}

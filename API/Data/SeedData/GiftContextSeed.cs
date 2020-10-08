72ca27a95f2af0c806cac688d02feef4e3040bca
Claudiu <cduneata@gmail.com>
2020-10-08 11:52:13 +0300

    Add json data

---
+++ API/Data/SeedData/GiftContextSeed.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using API.Contexts;
+using API.Entities;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Logging;
+
+namespace API.Data.SeedData
+{
+    public class GiftContextSeed
+    {
+        public static async Task SeedAsync(GiftContext context, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!context.Categories.Any())
+                {
+                    var categoryData = File.ReadAllText(@"..\API\Data\SeedData\categories.json");
+                    var categories = JsonSerializer.Deserialize<List<Category>>(categoryData);
+
+                    foreach (var category in categories)
+                    {
+                        context.Categories.Add(category);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+                if (!context.Products.Any())
+                {
+                    var productData = File.ReadAllText(@"..\API\Data\SeedData\products.json");
+                    var products = JsonSerializer.Deserialize<List<Product>>(productData);
+
+                    foreach (var product in products)
+                    {
+                        context.Products.Add(product);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                loggerFactory.CreateLogger<GiftContext>().LogError("Error adding categories to db");
+            }
+        }
+    }
+}

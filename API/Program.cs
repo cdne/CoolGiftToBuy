c8c75e2cb1e46c928704c45a66b29160336ad7ff
Claudiu <cduneata@gmail.com>
2020-10-08 11:48:51 +0300

    Add database migrations on startup

---
+++ API/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Contexts;
+using API.Data.SeedData;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace API
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var service = scope.ServiceProvider;
+                var loggerFactory = service.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context = service.GetRequiredService<GiftContext>();
+                    await context.Database.MigrateAsync();
+                    await GiftContextSeed.SeedAsync(context, loggerFactory);
+                }
+                catch(Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error ocurred during migrations");
+                }
+            }
+            await host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}

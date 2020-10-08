dcacf93953094eb3773bf30f8d8c9387b45581d1
Claudiu <cduneata@gmail.com>
2020-10-08 10:04:57 +0300

    Add category entity

---
+++ API/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+        public ICollection<Product> Product { get; set; } = new List<Product>();
+    }
+}

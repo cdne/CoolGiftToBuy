2466ab66fff858a2cfd77a721cec64de6dc93272
Claudiu <cduneata@gmail.com>
2020-10-08 11:52:53 +0300

    Add tag repository pattern

---
+++ API/Services/Tag/ITagRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using API.Entities;
+using API.Models;
+
+namespace API.Services
+{
+    public interface ITagRepository
+    {
+        ICollection<Tag> GetAllTags();
+        Tag GetTagById(int id);
+        IQueryable<ProductDto> GetAllProductsByTag(int tagId);
+    }
+}
---
+++ API/Services/Tag/TagRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using API.Contexts;
+using API.Entities;
+using API.Models;
+
+namespace API.Services
+{
+    public class TagRepository : ITagRepository
+    {
+        private readonly GiftContext _context;
+
+        public TagRepository(GiftContext context)
+        {
+            _context = context;
+        }
+        
+        public ICollection<Tag> GetAllTags()
+        {
+            return _context.Tags.ToList();
+        }
+
+        public Tag GetTagById(int id)
+        {
+            return _context.Tags.Find(id);
+        }
+
+        public IQueryable<ProductDto> GetAllProductsByTag(int tagId)
+        {
+            throw new System.NotImplementedException();
+        }
+        
+    }
+}

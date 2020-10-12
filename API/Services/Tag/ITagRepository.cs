a40b62eb6957d7c7d508f04a4304ab7bfca4dd66
Claudiu <cduneata@gmail.com>
2020-10-12 11:48:44 +0300

    Add tag method declaration

--- API/Services/Tag/ITagRepository.cs
+++ API/Services/Tag/ITagRepository.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using API.Entities;
@@ -12,5 +12,6 @@ namespace API.Services
         Tag GetTagById(int id);
         IQueryable<ProductDto> GetAllProductsByTag(int tagId);
 
+        void Add(Tag tag);
     }
 }

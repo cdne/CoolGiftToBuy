d629a3528f5375b8d7c713343aacbc8a29325245
Claudiu <cduneata@gmail.com>
2020-10-13 16:08:07 +0300

    Add documentation for CRUD operations on product

--- API/Services/Product/IProductRepository.cs
+++ API/Services/Product/IProductRepository.cs
@@ -8,17 +8,40 @@ namespace API.Services
 {
     public interface IProductRepository
     {
-        ICollection<Product> GetProducts();
-
+        /// <summary>
+        /// Get all products from database using search, sort or no queries
+        /// </summary>
+        /// <param name="name">Search query by name</param>
+        /// <param name="description">Search query by description</param>
+        /// <param name="sortName">Sort name asc or desc</param>
+        /// <returns>Collection of products based on search, sort and non queries</returns>
         ICollection<Product> GetProducts(string name, string description, string sortName);
         
+        /// <summary>
+        /// Get product from database by id
+        /// </summary>
+        /// <param name="id">Product id</param>
+        /// <returns>Product entity from database</returns>
         Product GetProductById(int id);
 
+        /// <summary>
+        /// Add product in database
+        /// </summary>
+        /// <param name="product">Product to add in database</param>
         void AddProduct(Product product);
         IQueryable<Tag> GetProductTags(int productId);
 
+        /// <summary>
+        /// Update product in database
+        /// </summary>
+        /// <param name="id">Product id</param>
+        /// <param name="product">Product entity to update</param>
         void UpdateProduct(int id, Product product);
 
+        /// <summary>
+        /// Partially update product in database
+        /// </summary>
+        /// <param name="product">Product entity to update</param>
         void Update(Product product);
 
         /// <summary>
@@ -27,6 +50,11 @@ namespace API.Services
         /// <param name="id">id of the product</param>
         void DeleteProductFromDatabase(int id);
         
+        /// <summary>
+        /// Get all tags for a product by product id
+        /// </summary>
+        /// <param name="productId">Product id</param>
+        /// <returns>All tags for that product</returns>
         IQueryable<Tag> GetTagsByProductId(int productId);
 
 

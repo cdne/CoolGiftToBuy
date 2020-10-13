cf3306a35a30799080f1e584670eb2a8b5c5349d
Claudiu <cduneata@gmail.com>
2020-10-13 16:14:24 +0300

    Add tag CRUD documentation

--- API/Services/Tag/ITagRepository.cs
+++ API/Services/Tag/ITagRepository.cs
@@ -8,13 +8,37 @@ namespace API.Services
 {
     public interface ITagRepository
     {
+        /// <summary>
+        /// Get all tags from database 
+        /// </summary>
+         /// <returns>Collection of Tag entities</returns>
         ICollection<Tag> GetAllTags();
+        /// <summary>
+        /// Get tag from database by id
+        /// </summary>
+        /// <param name="id">Tag id</param>
+        /// <returns>Tag from database</returns>
         Tag GetTagById(int id);
-        IQueryable<ProductDto> GetAllProductsByTag(int tagId);
-
+        /// <summary>
+        /// Add tag in database
+        /// </summary>
+        /// <param name="tag">Tag entity to add</param>
         void Add(Tag tag);
+        /// <summary>
+        /// Update tag in database
+        /// </summary>
+        /// <param name="id">Tag id to update</param>
+        /// <param name="tag">Tag data to update</param>
         void Update(int id, Tag tag);
+        /// <summary>
+        /// Partially update tag in database
+        /// </summary>
+        /// <param name="tag">Tag entity to update</param>
         void PartiallyUpdate(Tag tag);
+        /// <summary>
+        /// Delete tag from database
+        /// </summary>
+        /// <param name="tag">Tag entity to delete</param>
         void Delete(Tag tag);
 
     }

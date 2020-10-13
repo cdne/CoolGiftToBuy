af3c3f701543d7a8514636c1b19e3b4ff3e81dea
Claudiu <cduneata@gmail.com>
2020-10-13 16:54:04 +0300

    Add category controller documentation

--- API/Controllers/CategoriesController.cs
+++ API/Controllers/CategoriesController.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Logging;
 
 namespace API.Controllers
 {
+    /// <summary>
+    /// Controller for categories
+    /// </summary>
     [Route("api/v{version:apiVersion}/categories")]
     [ApiVersion("1.0")]
     [ApiVersion("1.1")]
@@ -27,6 +30,10 @@ namespace API.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Get all categories on HttpGet request
+        /// </summary>
+        /// <returns>Collection of categories</returns>
         [HttpGet]
         [MapToApiVersion("1.0")]
         [MapToApiVersion("1.1")]
@@ -37,6 +44,11 @@ namespace API.Controllers
         }
         
 
+        /// <summary>
+        /// Get category by id on HttpGet request
+        /// </summary>
+        /// <param name="id">Category id</param>
+        /// <returns>Category by id</returns>
         [HttpGet("{id}")]
         [MapToApiVersion("1.0")]
         [MapToApiVersion("1.1")]
@@ -46,6 +58,11 @@ namespace API.Controllers
             return Ok(_mapper.Map<CategoryDto>(category));
         }
 
+        /// <summary>
+        /// Get all products from a category by category id on HttpGet request
+        /// </summary>
+        /// <param name="id">Category id</param>
+        /// <returns>Collection with products by category id</returns>
         [HttpGet("{id}/products")]
         [MapToApiVersion("1.0")]
         [MapToApiVersion("1.1")]
@@ -55,6 +72,11 @@ namespace API.Controllers
             return Ok(_mapper.Map<ICollection<ProductDto>>(productsByCategory));
         }
 
+        /// <summary>
+        /// Add category on HttpPost request
+        /// </summary>
+        /// <param name="category">Category mapped from body</param>
+        /// <returns>Created category</returns>
         [HttpPost]
         [MapToApiVersion("1.1")]
         public IActionResult AddCategory([FromBody] CategoryForCreationDto category)
@@ -73,6 +95,12 @@ namespace API.Controllers
                 categoryToReturn);
         }
 
+        /// <summary>
+        /// Update category from HttpPut request
+        /// </summary>
+        /// <param name="id">Category id</param>
+        /// <param name="categoryDto">Category DTO</param>
+        /// <returns>Updated category</returns>
         [HttpPut("{id}")]
         [MapToApiVersion("1.1")]
         public IActionResult UpdateCategory(int id, [FromBody] CategoryDto categoryDto)
@@ -89,6 +117,12 @@ namespace API.Controllers
                 categoryToReturn);
         }
 
+        /// <summary>
+        /// Update category on HttpPatch request
+        /// </summary>
+        /// <param name="id">Category id</param>
+        /// <param name="patchCategory">JsonPatchDocument object</param>
+        /// <returns>Updated category</returns>
         [HttpPatch("{id}")]
         [MapToApiVersion("1.1")]
         public IActionResult PartiallyUpdateCategory(int id, [FromBody] JsonPatchDocument<CategoryDto> patchCategory)
@@ -108,6 +142,12 @@ namespace API.Controllers
                 categoryDto);
         }
 
+        
+        /// <summary>
+        /// Remove category on HttpDelete request
+        /// </summary>
+        /// <param name="id">Category id</param>
+        /// <returns>"Category was deleted" string on successful request</returns>
         [HttpDelete("{id}")]
         public IActionResult DeleteCategory(int id)
         {
